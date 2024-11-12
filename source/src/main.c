670d1182d9582030cce03c5dc9861f36972b56f5
Jakub Kováč <114470358+247828@users.noreply.github.com>
2024-11-12 21:03:53 +0100

    Aktualizácia funkcie updAngle
    Zobrazovanie uhla s desatiným čislom.

--- source/src/main.c
+++ source/src/main.c
@@ -10,7 +10,7 @@
 #include <lcd.h>            // Peter Fleury's LCD library
 #include <uart.h>           // Peter Fleury's UART library
 #include <stdlib.h>         // C library. Needed for number conversions
-#include <math.h> // C library abs()
+#include <math.h>           // C library fabs()
 
 // -- Global variables -- //
 // "Flags"
@@ -22,15 +22,18 @@ volatile uint8_t mainScreen = 0;
 
 char print[3]; // itoa
 
+float angleFloat = 0;
+
 struct Angle_structure {
     int8_t aInt;    // Integer part
     uint8_t aDec;   // Decimal part
-    uint8_t negDec; // negative decimal part from -0.1 to -0.9
+    uint8_t aSign; // Sign (0 positive or 1 negative)
 } angle;
 /*
 struct Height_structure {
     int8_t hInt;    // Integer part
     uint8_t hDec;   // Decimal part
+    uint8_t hSign; // Sign (0 positive or 1 negative)
 } height;
 */
 
@@ -40,78 +43,138 @@ struct Height_structure {
  * Purpose:  Update the screen with new angle information and graphical represantation.
  * Returns:  none
  */
-void updAngle(int8_t aInt, uint8_t aDec, uint8_t signDec) {
+void updAngle() {
   static int8_t barPosition = 0;
-  static uint8_t absBarPoisition = 0;
+  // float to dec
+  if (angleFloat < 0) 
+  {
+    angle.aSign = 1; 
+  }
+  else
+  {
+    angle.aSign = 0;
+  }
+  angle.aInt = fabs(angleFloat);
+  angle.aDec = (fabs(angleFloat) - angle.aInt) * 10;
+  /*
   uart_puts("New angle: ");
-      itoa(abs(aInt), print, 10);
-      uart_puts(print);
-      uart_puts("\r\n");
-      //uart_puts(decString);
-      //uart_puts("\r\n");
-      
-      if(aInt < 0) {
-        if(aInt > -10) {
-          lcd_gotoxy(10, 0);
-          lcd_puts(" -");
-          lcd_puts(print);
-          //lcd_putc(',');
-          //lcd_putc(decString[0]);
-        }
-        else {
-          lcd_gotoxy(10, 0);
-          lcd_putc('-');
-          lcd_puts(print);
-          //lcd_putc(',');
-          //lcd_putc(decString[0]);
-        }
-      }
-      else {
-        /*if(angle.negDec == 1) 
-        {
-          lcd_gotoxy(10, 0);
-          lcd_puts(" -0,");
-          lcd_putc(decString[0]);
-        }*/
-        if(aInt < 10) {
-          lcd_gotoxy(10, 0);
-          lcd_puts("  ");
-          lcd_puts(print);
-          //lcd_putc(',');
-          //lcd_putc(decString[0]);
-        }
-        else {
-          lcd_gotoxy(10, 0);
-          lcd_putc(' ');
-          lcd_puts(print);
-          //lcd_putc(',');
-          //lcd_putc(decString[0]);
-        }
-      }
-      // Bar position
-      if(aInt <= -6) barPosition = (aInt+6)/30 - 1;
-      else if(aInt >= 6) barPosition = (aInt-6)/30 + 1;
-      else barPosition = 0;
-      absBarPoisition = abs(barPosition);
-      uart_puts("Bar position: ");
-      itoa(barPosition, print, 10);
-      uart_puts(print);
-      uart_puts("\r\n");
+  itoa(angle.aSign, print, 10);
+  uart_puts(print);
+  uart_putc(' ');
+  itoa(angle.aInt, print, 10);
+  uart_puts(print);
+  uart_putc(',');
+  itoa(angle.aDec, print, 10);
+  uart_puts(print);
+  uart_puts("\r\n");
+  */
+  if (angle.aSign == 1) 
+  {
+    if(angle.aInt >= 10)
+    {
+      lcd_gotoxy(10, 0);
+      lcd_putc('-');
+      itoa(angle.aInt, print, 10);
+      lcd_puts(print);
+      lcd_gotoxy(14, 0);
+      itoa(angle.aDec, print, 10);
+      lcd_puts(print);
+    }
+    else
+    {
+      lcd_gotoxy(10, 0);
+      lcd_puts(" -");
+      itoa(angle.aInt, print, 10);
+      lcd_puts(print);
+      lcd_gotoxy(14, 0);
+      itoa(angle.aDec, print, 10);
+      lcd_puts(print);
+    }
+  }
+  else 
+  {
+      if(angle.aInt >= 10)
+    {
+      lcd_gotoxy(10, 0);
+      lcd_putc(' ');
+      itoa(angle.aInt, print, 10);
+      lcd_puts(print);
+      lcd_gotoxy(14, 0);
+      itoa(angle.aDec, print, 10);
+      lcd_puts(print);
+    }
+    else
+    {
+      lcd_gotoxy(10, 0);
+      lcd_puts("  ");
+      itoa(angle.aInt, print, 10);
+      lcd_puts(print);
+      lcd_gotoxy(14, 0);
+      itoa(angle.aDec, print, 10);
+      lcd_puts(print);
+    }
+  }
+  // Bar position
+  if(angle.aInt >= 6)
+  { 
+    barPosition = (angle.aInt-6)/30 + 1;
+  }
+  else
+  {
+    barPosition = 0;
+  }
+  /*
+  uart_puts("Bar position: ");
+  itoa(barPosition, print, 10);
+  uart_puts(print);
+  uart_puts("\r\n");
+  */
+  lcd_gotoxy(1, 0);
+  lcd_puts("   ");
+  lcd_putc(4);
+  lcd_puts("   ");
+  if (angle.aSign == 0)
+  {
+    lcd_gotoxy(4+barPosition, 0);
+    /*
+    itoa(4+barPosition, print, 10);
+    uart_puts(print);
+    uart_puts("\r\n");
+    */
 
-      lcd_gotoxy(1, 0);
-      lcd_puts("   ");
-      lcd_putc(4);
-      lcd_puts("   ");
-      lcd_gotoxy(4+barPosition, 0);
-      
-      if(aInt < 6 && aInt > -6) lcd_putc('I');
-      else if(aInt <= (-6)-(absBarPoisition-1)*30 && aInt > (-12)-(absBarPoisition-1)*30 || aInt >= 30+(absBarPoisition-1)*30 && aInt < 36+(absBarPoisition-1)*30) lcd_putc(3);
-      else if(aInt <= (-12)-(absBarPoisition-1)*30 && aInt > (-18)-(absBarPoisition-1)*30 || aInt >= 24+(absBarPoisition-1)*30 && aInt < 30+(absBarPoisition-1)*30) lcd_putc(2);
-      else if(aInt <= (-18)-(absBarPoisition-1)*30 && aInt > (-24)-(absBarPoisition-1)*30 || aInt >= 18+(absBarPoisition-1)*30 && aInt < 24+(absBarPoisition-1)*30) lcd_putc('|');
-      else if(aInt <= (-24)-(absBarPoisition-1)*30 && aInt > (-30)-(absBarPoisition-1)*30 || aInt >= 12+(absBarPoisition-1)*30 && aInt < 18+(absBarPoisition-1)*30) lcd_putc(1);
-      else if(aInt <= (-30)-(absBarPoisition-1)*30 && aInt > (-36)-(absBarPoisition-1)*30 || aInt >= 6+(absBarPoisition-1)*30 && aInt < 12+(absBarPoisition-1)*30) lcd_putc(0);
-
-      updateAngle--;
+  }
+  else
+  {
+    lcd_gotoxy(4-barPosition, 0);
+    /*
+    itoa(4-barPosition, print, 10);
+    uart_puts(print);
+    uart_puts("\r\n");
+    */
+  }
+  //
+  if(angle.aInt < 6 && angle.aInt > -6) lcd_putc('I');
+  else if((angleFloat <= (-6)-(barPosition-1)*30 && angleFloat > (-12)-(barPosition-1)*30) || (angleFloat >= 30+(barPosition-1)*30 && angleFloat < 36+(barPosition-1)*30)) 
+  {
+    lcd_putc(3);
+  }
+  else if((angleFloat <= (-12)-(barPosition-1)*30 && angleFloat > (-18)-(barPosition-1)*30) || (angleFloat >= 24+(barPosition-1)*30 && angleFloat < 30+(barPosition-1)*30)) 
+  {
+    lcd_putc(2);
+  }
+  else if((angleFloat <= (-18)-(barPosition-1)*30 && angleFloat > (-24)-(barPosition-1)*30) || (angleFloat >= 18+(barPosition-1)*30 && angleFloat < 24+(barPosition-1)*30))
+  { 
+    lcd_putc('|');
+  }
+  else if((angleFloat <= (-24)-(barPosition-1)*30 && angleFloat > (-30)-(barPosition-1)*30) || (angleFloat >= 12+(barPosition-1)*30 && angleFloat < 18+(barPosition-1)*30)) 
+  {
+    lcd_putc(1);
+  }
+  else if((angleFloat <= (-30)-(barPosition-1)*30 && angleFloat > (-36)-(barPosition-1)*30) || (angleFloat >= 6+(barPosition-1)*30 && angleFloat < 12+(barPosition-1)*30)) 
+  {
+    lcd_putc(0);
+  }
+  updateAngle--;
 }
 
 /*
@@ -123,20 +186,19 @@ void updAngle(int8_t aInt, uint8_t aDec, uint8_t signDec) {
 int main(void)
 {
   // -- Local variables -- //
-
+  // Custom characters
   uint8_t leftBar[8] = {0x10,0x10,0x10,0x10,0x10,0x10,0x10,0x0};
   uint8_t leftCenterBar[8] = {0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x0};
   uint8_t rightCenterBar[8] = {0x2,0x2,0x2,0x2,0x2,0x2,0x2,0x0};
   uint8_t rightBar[8] = {0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x0};
   uint8_t center[8] = {0xa,0x0,0x0,0x0,0x0,0x0,0xa,0x0};
 
-
   // Configure  Timers/Counters
   TIM0_ovf_1ms();      // Set prescaler to 1 sec and enable overflow interrupt
   TIM1_ovf_1sec();      // Set prescaler to 1 sec and enable overflow interrupt
 
   // Enable overflow interrupt
-  TIM1_ovf_enable();
+  //TIM1_ovf_enable();
   // Interrupts must be enabled, bacause of `uart_puts()`
   sei();
 
@@ -154,7 +216,7 @@ int main(void)
   lcd_clrscr();
   lcd_home();
   uart_puts("LCD initialized.\r\n");
-
+  /*
   // Welcome screen
   lcd_puts("PROJEKT BPC-DE2.");
   lcd_gotoxy(0,1);
@@ -180,6 +242,7 @@ int main(void)
   mainScreen--;
   lcd_clrscr();
   lcd_home();
+  */
   // Main screen
   lcd_puts("I   ");
   lcd_putc(4);
@@ -188,13 +251,14 @@ int main(void)
   lcd_gotoxy(0,1);
   lcd_puts("          -00,0m");
   uart_puts("Main scr.\r\n");
-  angle.aInt = 95;
+  
+  angleFloat = -96;
   TIM0_ovf_enable();
   uart_puts("Angle set 90d and TCNT0 enabled.\r\n");
   // Infinite loop
   while (1)
   {
-    if(updateAngle == 1) updAngle(angle.aInt, angle.aDec, angle.negDec);
+    if(updateAngle == 1) updAngle();
   }
   return 0;
 }
@@ -211,13 +275,13 @@ ISR(TIMER0_OVF_vect)
   ovfs++;
   if(ovfs >= 50)
   {
-    if (angle.aInt >= 95) {
-      angle.aInt = -95;
+    if (angleFloat >= 95.9) {
+      angleFloat = -95.9;
     }
     else {
-      angle.aInt++;
+      angleFloat += 0.1;
     }
-  uart_puts("TCNT0: 50 ms has passed.\r\n");
+  //uart_puts("TCNT0: 100 ms has passed.\r\n");
   updateAngle++;
   ovfs = 0;
   }
